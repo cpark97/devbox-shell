dab13a0d04cecdaaf5172fa3e16fc3f917c3a267
Park Changbeom <eric9710@naver.com>
2025-08-09 03:18:03 +0900

    chore: init nix flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Temporary devbox shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      # https://ayats.org/blog/no-flake-utils
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ] (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (pkgs: {
+        default = pkgs.writeShellScriptBin "devbox-shell" (builtins.readFile ./devbox-shell);
+      });
+    };
+}
